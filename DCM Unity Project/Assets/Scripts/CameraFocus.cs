62c952b220b5043ae2bf63a437de88c0ce9a98a4
nielss94 <nielsvdam@hotmail.com>
2015-10-13 14:35:01 +0200

    Camera follows player(quick fix)

---
+++ DCM Unity Project/Assets/Scripts/CameraFocus.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFocus : MonoBehaviour {
+
+    private GameObject target;
+
+	void Start () {
+        target = GameObject.Find("Player");
+	}
+	
+
+	void Update () {
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y + 50, target.transform.position.z);
+	}
+}
