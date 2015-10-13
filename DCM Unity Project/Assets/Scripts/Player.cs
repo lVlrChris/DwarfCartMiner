81e0690740ea30b46a72075e18f9d66d31d61b8f
nielss94 <nielsvdam@hotmail.com>
2015-10-13 13:04:06 +0200

    Nieuwe scene + iTween

---
+++ DCM Unity Project/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        MoveOnPathLine("LevelPathLine", iTween.EaseType.linear, 15);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void MoveOnPathLine(string pathLineName, iTween.EaseType easetype, float time)
+    {
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath(pathLineName), "easetype", easetype, "time", time, "orientToPath", true, "lookahead", 0.01f));
+    }
+}
