ee44f5e684f2236d98018438370b5181482f8f86
nielss94 <nielsvdam@hotmail.com>
2015-10-15 12:49:25 +0200

    Weapon and look at mouse.

--- DCM Unity Project/Assets/Scripts/Player.cs
+++ DCM Unity Project/Assets/Scripts/Player.cs
@@ -15,6 +15,8 @@ public class Player : MonoBehaviour {
 
 	void Update () {
         previousPos.Add(transform.position);
+
+
 	}
 
     private void MoveOnPathLine(string pathLineName, iTween.EaseType easetype, float time)
---
+++ DCM Unity Project/Assets/Scripts/Weapon.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+
+    private RaycastHit hit;
+    private float rayDistance = 10000.0f;
+
+
+    void Start () {
+	
+	}
+	
+
+	void Update () {
+        GetInput();
+	}
+
+    private void GetInput()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            LookAtMouse();
+        }
+    }
+
+    private void LookAtMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, rayDistance))
+        {
+            transform.LookAt(hit.point);
+            transform.rotation = new Quaternion(0.0f, transform.rotation.y, 0.0f, transform.rotation.w);
+        }
+    }
+}
